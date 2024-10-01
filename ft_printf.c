5e71826026c22cf47f7952c8aa402bc9eda790ce
retakashi <119090016+retakashi@users.noreply.github.com>
2024-10-01 18:25:21 +0900

    fix:va_list apをポインタに修正 なぜ今ごろ、、、

--- ft_printf.c
+++ ft_printf.c
@@ -3,40 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rtakashi <rtakashi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: reira <reira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:47:00 by rtakashi          #+#    #+#             */
-/*   Updated: 2023/03/07 18:33:49 by rtakashi         ###   ########.fr       */
+/*   Updated: 2024/10/01 18:23:43 by reira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+#include <stdio.h>
 static int	print_char(int c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
 }
 
-static int	find_type(char *fmt, size_t i, va_list ap)
+int	find_type(char *fmt, size_t i, va_list *ap)
 {
 	int	ans;
 
 	ans = 0;
 	if (fmt[i] == 'c')
-		ans += print_char((int)va_arg(ap, int));
+		ans += print_char((int)va_arg(*ap, int));
 	else if (fmt[i] == 's')
-		ans += print_str((char *)va_arg(ap, char *));
+		ans += print_str((char *)va_arg(*ap, char *));
 	else if (fmt[i] == 'p')
-		ans += print_ptr((unsigned long long)va_arg(ap, void *));
+		ans += print_ptr((unsigned long long)va_arg(*ap, void *));
 	else if (fmt[i] == 'd' || fmt[i] == 'i')
-		ans += print_int((int)va_arg(ap, int));
+		ans += print_int((int)va_arg(*ap, int));
 	else if (fmt[i] == 'u')
-		ans += print_un_int((unsigned int)va_arg(ap, unsigned int));
+		ans += print_un_int((unsigned int)va_arg(*ap, unsigned int));
 	else if (fmt[i] == 'x')
-		ans += print_hex((unsigned int)va_arg(ap, unsigned int), 1);
+		ans += print_hex((unsigned int)va_arg(*ap, unsigned int), 1);
 	else if (fmt[i] == 'X')
-		ans += print_hex((unsigned int)va_arg(ap, unsigned int), 2);
+		ans += print_hex((unsigned int)va_arg(*ap, unsigned int), 2);
 	else
 		return (ft_putchar_fd(fmt[i], 1));
 	return (ans);
@@ -56,7 +56,7 @@ int	ft_printf(const char *fmt, ...)
 		if (fmt[i] == '%')
 		{
 			i++;
-			ans += find_type((char *)fmt, i, ap);
+			ans += find_type((char *)fmt, i, &ap);
 		}
 		else
 		{
@@ -74,19 +74,19 @@ int	ft_printf(const char *fmt, ...)
 //     system("leaks -q a.out");
 // }
 
-// #include <stdio.h>
-// int	main(void)
-// {
-// 	int		ans1;
-// 	int		ans2;
-// 	char	*p;
+#include <stdio.h>
+int	main(void)
+{
+	int		ans1;
+	int		ans2;
+	char	*p;
 
-// 	p = "12348";
-// 	ans1 = printf("%!123\n");
-// 	ans2 = ft_printf("%!123\n");
-// 	ans1 = printf("%d %c %s %p\n", INT_MAX, 'a', p, p);
-// 	ans2 = ft_printf("%d %c %s %p\n", INT_MAX, 'a', p, p);
-// 	printf("ans1: %d\n", ans1);
-// 	ft_printf("ans2: %d\n", ans2);
-// 	return (0);
-// }
+	p = "123hello, world";
+	ans1 = printf("!123\n");
+	ans2 = ft_printf("!123\n");
+	ans1 = printf("%d %c %s %p\n", INT_MAX, 'a', p, p);
+	ans2 = ft_printf("%d %d %s %p\n", INT_MAX, 5, p, p);
+	printf("ans1: %d\n", ans1);
+	ft_printf("ans2: %d\n", ans2);
+	return (0);
+}
